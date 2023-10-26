649a648849853545a8f0dcc16bbc528b0a81f4a3
Anastasia <dzheyn.anna@mail.ru>
2023-10-26 16:30:19 +0400

    add friends wip#2

--- backend/apps/users/src/app/friends/friends.controller.ts
+++ backend/apps/users/src/app/friends/friends.controller.ts
@@ -1,8 +1,51 @@
-import { Controller } from '@nestjs/common';
-import { API_TAG_NAME } from './friends.constant';
+import { Controller, Delete, Get, HttpStatus, Param, Post, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import { API_TAG_NAME, FriendsMessages, FriendsPath } from './friends.constant';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@backend/util/util-core';
+import { RequestWithUserPayload } from '@backend/shared/shared-types';
+import { UserRoleInterceptor } from '../user-info/interceptors/user-role.interceptor';
+import { FriendsService } from './friends.service';
 
-@Controller(API_TAG_NAME)
+@ApiTags(API_TAG_NAME)
+@Controller(FriendsPath.Main)
 export class FriendsController {
+  constructor(
+    private readonly friendsService: FriendsService,
+  ) {}
 
-  
+  @ApiResponse({
+    status:HttpStatus.CREATED,
+    description: FriendsMessages.Add
+  })
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(UserRoleInterceptor)
+  @Post(FriendsPath.Id)
+  public async addFriend( @Param('id') id:number, @Req() {user}: RequestWithUserPayload) {
+    const userId = user.sub;
+    const newFriend = await this.friendsService.addFriend(id,userId);
+    return newFriend;
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: FriendsMessages.Show,
+  })
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  public async showFriends(@Req() {user}: RequestWithUserPayload) {
+    const{sub, role} = user;
+    const friendsInfo = await this.friendsService.showFriends(sub,role);
+    return friendsInfo;
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: FriendsMessages.Remove,
+  })
+  @UseGuards(JwtAuthGuard)
+  @Delete(FriendsPath.Id)
+  public async deleteFriend(@Param('id') id:number, @Req() {user}: RequestWithUserPayload) {
+    const userId = user.sub;
+    return await this.friendsService.removeFriend(id, userId);
+  }
 }
--- backend/apps/users/src/app/friends/friends.module.ts
+++ backend/apps/users/src/app/friends/friends.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
 import { FriendsRepository } from './friends.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtAccessStrategy, getJwtOptions } from '@backend/util/util-core';
 
 @Module({
-  providers: [FriendsService, FriendsRepository],
+  imports:[
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    })
+  ],
+  providers: [FriendsService, FriendsRepository, JwtAccessStrategy],
   controllers: [FriendsController],
 })
 export class FriendsModule {}
