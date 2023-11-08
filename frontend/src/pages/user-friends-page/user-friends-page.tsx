01e69e3fba35df97ecdfcc636c2f57d0fb83fd98
Anastasia <dzheyn.anna@mail.ru>
2023-11-09 02:39:57 +0400

    add pages wip#4

--- frontend/src/pages/user-friends-page/user-friends-page.tsx
+++ frontend/src/pages/user-friends-page/user-friends-page.tsx
@@ -1,8 +1,34 @@
+import FriendCard from '../../components/friend-card/friend-card';
+import Header from '../../components/header/header';
+import ShowMoreButton from '../../components/show-more-button/show-more-button';
 
 function UserFriendsPage(): JSX.Element {
   return (
-    <>
-    </>
+    <div className="wrapper">
+      <Header/>
+      <main>
+        <section className="friends-list">
+          <div className="container">
+            <div className="friends-list__wrapper">
+              <button className="btn-flat friends-list__back" type="button">
+                <svg width="14" height="10" aria-hidden="true">
+                  <use xlinkHref="#arrow-left"></use>
+                </svg><span>Назад</span>
+              </button>
+              <div className="friends-list__title-wrapper">
+                <h1 className="friends-list__title">Мои друзья</h1>
+              </div>
+              <ul className="friends-list__list">
+                <li className="friends-list__item">
+                  <FriendCard/>
+                </li>
+              </ul>
+              <ShowMoreButton/>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 export default UserFriendsPage;
--- frontend/src/pages/users-list-page/users-list-page.tsx
+++ frontend/src/pages/users-list-page/users-list-page.tsx
@@ -1,8 +1,37 @@
+import UsersListFilters from '../../components/filters/users-list-filters';
+import Header from '../../components/header/header';
+import UserCardSmall from '../../components/user-card/user-card-small';
 
 function UsersListPage(): JSX.Element {
   return (
-    <>
-    </>
+    <div className="wrapper">
+      <Header/>
+      <main>
+        <section className="inner-page">
+          <div className="container">
+            <div className="inner-page__wrapper">
+              <h1 className="visually-hidden">Каталог пользователей</h1>
+              <UsersListFilters/>
+              <div className="inner-page__content">
+                <div className="users-catalog">
+                  <ul className="users-catalog__list">
+                    <li className="users-catalog__item">
+                      <UserCardSmall/>
+                    </li>
+                  </ul>
+                  <div className="show-more users-catalog__show-more">
+                    <button className="btn show-more__button show-more__button--more" type="button">Показать еще</button>
+                    <button className="btn show-more__button show-more__button--to-top" type="button">Вернуться в
+                    начало
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
 export default UsersListPage;
